eac960b1f2c50b0f603a1b041942085378662b37
ksky521 <ksky521@gmail.com>
2021-05-21 10:04:00 +0800

    :bug: backend.js去掉merge-stream实现

--- server/middlewares/backend.js
+++ server/middlewares/backend.js
@@ -1,17 +1,27 @@
 const fs = require('fs');
 const path = require('path');
-const mergeStream = require('merge-stream');
+// const mergeStream = require('merge-stream');
 
 const distPath = path.join(__dirname, '../../dist');
 module.exports = (router, logger, serverInstance) => {
     const log = logger.withTag('middle:backend');
+    const beFilepath = path.join(distPath, ctx.path);
     router.get('/backend.js', async (ctx, next) => {
-        const mergedStream = mergeStream(fs.createReadStream(path.join(distPath, ctx.path)));
+        let s = fs.readFileSync(beFilepath).toString();
+
         const backendFiles = serverInstance._backends || [];
         backendFiles.forEach(filepath => {
-            log.debug(`add ${filepath}`);
-            mergedStream.add(fs.createReadStream(filepath));
+            s += fs.readFileSync(filepath).toString();
         });
-        ctx.body = mergedStream;
+
+        // const mergedStream = mergeStream(fs.createReadStream(beFilepath));
+        // const backendFiles = serverInstance._backends || [];
+        // backendFiles.forEach(filepath => {
+        //     log.debug(`add ${filepath}`);
+        //     mergedStream.add(fs.createReadStream(filepath));
+        // });
+        // console.log(mergedStream.toString());
+        // ctx.body = mergedStream;
+        ctx.body = s;
     });
 };
