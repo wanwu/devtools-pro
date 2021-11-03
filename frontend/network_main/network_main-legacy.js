7ab636505f4a8d97d85c58e2a44aaf40f6e4ca70
wangyongqing01 <wangyongqing01@baidu.com>
2021-11-03 13:18:30 +0800

    :boom: network单独面板调通

---
+++ frontend/network_main/network_main-legacy.js
@@ -0,0 +1,13 @@
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import * as NetworkMainModule from './network_main.js';
+
+self.NetworkMain = self.NetworkMain || {};
+NetworkMain = NetworkMain || {};
+
+/**
+ * @constructor
+ */
+NetworkMain.NetworkMain = NetworkMainModule.NetworkMain.NetworkMainImpl;
